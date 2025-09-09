3b026a9a751e31f4bf74f3ef7e3c9593d1fe4ba8
Tim Morris <timorris@timorris.com>
2025-09-09 17:43:32 -0400

    added styling with css

--- web.py
+++ web.py
@@ -1,6 +1,13 @@
 import streamlit as st
 import functions as functions
 
+# Inject custom CSS
+def load_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+load_css("site.css")
+
 todos = functions.get_todos()
 
 def add_todo():
@@ -11,7 +18,9 @@ def add_todo():
 
 st.title("My Todo App")
 st.subheader("This is my todo app.")
-st.write('This app is to increase your productivity.')
+st.write('<span class="blurb">This app is to increase your&#160;'
+         '<strong style="font-size:1.2em;"><em>productivity</em></strong> (hopefully).</span>',
+         unsafe_allow_html=True)
 
 for index, todo in enumerate(todos):
     checkbox = st.checkbox(todo, key=todo)
@@ -20,7 +29,6 @@ for index, todo in enumerate(todos):
         functions.write_todos(todos)
         del st.session_state[todo]
         st.rerun()
-
 
 st.divider()
 st.text_input(label='', placeholder='Add new todo...',
