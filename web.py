2b9248d03f53825cbce7132e2874f94f29d1cdde
Tim Morris <timorris@timorris.com>
2025-09-09 15:17:55 -0400

    initial commit for web_todo_app

---
+++ web.py
@@ -0,0 +1,30 @@
+import streamlit as st
+import functions as functions
+
+todos = functions.get_todos()
+
+def add_todo():
+    new_todo = st.session_state['new_todo']
+    st.session_state['new_todo'] = ''
+    todos.append(new_todo + '\n')
+    functions.write_todos(todos)
+
+st.title("My Todo App")
+st.subheader("This is my todo app.")
+st.write('This app is to increase your productivity.')
+
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.rerun()
+
+
+st.divider()
+st.text_input(label='', placeholder='Add new todo...',
+              on_change=add_todo, key='new_todo')
+
+st.divider()
+st.session_state
